8291b1ca33a97e64ada4d55d2e076b2999eae267
Alexi Kostibas <alexi@kostibas.com>
2017-06-30 18:09:45 -0700

    start django server with backpack service

--- bin/backpack.sh
+++ bin/backpack.sh
@@ -4,6 +4,10 @@ set -eux -o pipefail
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 function stop() {
+    pushd /var/www/BackpackPi
+    make stop_django
+    popd
+
 	sysctl net.ipv4.ip_forward=0
 	pkill hostapd
 	ifconfig uap0 down
@@ -11,6 +15,10 @@ function stop() {
 }
 
 function start() {
+    pushd /var/www/BackpackPi
+    make start_django
+    popd
+
 	ifconfig uap0 || \
 		iw dev wlan0 interface add uap0 type __ap
 
